b3d8c5ca9278b83df8b0a83500e548ea027f2004
Stepan <st085873@gmail.com>
2020-10-11 23:54:51 +0300

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include "sort.h"
 
 #define INPUT_ARRAY_MAX_SIZE 100
+#define FROM_PARAM_STRING "--from="
+#define TO_PARAM_STRING "--to="
 #define stderr_printf(...) fprintf(stderr, __VA_ARGS__)
 
 //Параметры from и from_set_flag (to и to_set_flag) обновляются парами
@@ -20,18 +22,17 @@ int parse_argv(int argc, char* argv[], struct interval_t* out_interval) {
         return -1;
     if(argc > 3)
         return -2;
-    if(argc == 3 && strcmp(strtok(argv[1], "="), strtok(argv[2], "=")) == 0)
+    if(argc == 3 && strncmp(argv[1], argv[2], strcspn(argv[1], "=")) == 0)
         return -3;
 
     for(int i = 1; i < argc; i++) {
         long long number_in_string;
-        if(strncmp(argv[i], "--from", 6) == 0 && out_interval->from_set_flag == 0) {
-            number_in_string = strtoll(argv[i] + 7, NULL, 10);
+        if (strncmp(argv[i], FROM_PARAM_STRING, strlen(FROM_PARAM_STRING)) == 0 && out_interval->from_set_flag == 0) {
+            number_in_string = strtoll(strchr(argv[i], '=') + 1, NULL, 10);
             out_interval->from = number_in_string;
             out_interval->from_set_flag = 1;
-        }
-        else if(strncmp(argv[i], "--to", 4) == 0 && out_interval->to_set_flag == 0) {
-            number_in_string = strtoll(argv[i] + 5, NULL, 10);
+        } else if (strncmp(argv[i], TO_PARAM_STRING, strlen(TO_PARAM_STRING)) == 0 && out_interval->to_set_flag == 0) {
+            number_in_string = strtoll(strchr(argv[i], '=') + 1, NULL, 10);
             out_interval->to = number_in_string;
             out_interval->to_set_flag = 1;
         }
@@ -43,7 +44,7 @@ int parse_argv(int argc, char* argv[], struct interval_t* out_interval) {
     return 0;
 }
 
-int read_array(long long out_array[], int array_max_size, struct interval_t interval) {
+size_t read_input_array(long long out_array[], int array_max_size, struct interval_t interval) {
     int size = 0;
     char delim;
     long long num;
@@ -64,11 +65,6 @@ int read_array(long long out_array[], int array_max_size, struct interval_t inte
     return size;
 }
 
-void copy_array(const long long array_from[], long long array_to[], int size) {
-    for(int i = 0; i < size; i++)
-        array_to[i] = array_from[i];
-}
-
 int compare_arrays(const long long array1[], const long long array2[], int size) {
     int difference_count = 0;
     for(int i = 0; i < size; i++) {
@@ -86,16 +82,16 @@ int main(int argc, char* argv[]) {
     if(parse_return_code != 0)
         return parse_return_code;
 
-    int size;
-    long long array[INPUT_ARRAY_MAX_SIZE];
-    size = read_array(array, INPUT_ARRAY_MAX_SIZE, interval);
+    size_t size;
+    long long input_array[INPUT_ARRAY_MAX_SIZE];
+    size = read_input_array(input_array, INPUT_ARRAY_MAX_SIZE, interval);
 
-    long long *array_copy = (long long*)malloc(size * sizeof(long long));
-    if(array_copy == NULL)
+    long long *input_array_copy = (long long*)malloc(size * sizeof(long long));
+    if(input_array_copy == NULL)
         exit(EXIT_FAILURE);
-    copy_array(array, array_copy, size);
-    sort(array_copy, array_copy + size);
-    int swapped_count = compare_arrays(array, array_copy, size);
+    memcpy(input_array_copy, input_array, size * sizeof(long long));
+    sort(input_array_copy, input_array_copy + size);
+    int swapped_count = compare_arrays(input_array, input_array_copy, size);
 
     return swapped_count;
 }
