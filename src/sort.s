2a2c5edf87a4ab6e7d4b86b06a7df5d8e10e596a
Stepan <st085873@gmail.com>
2020-10-13 01:30:35 +0300

    modified:   src/sort.s

--- src/sort.s
+++ src/sort.s
@@ -1,69 +1,104 @@
 	.file	"sort.c"
 	.text
 	.globl	sort
-	.def	sort;	.scl	2;	.type	32;	.endef
-	.seh_proc	sort
+	.type	sort, @function
 sort:
-	pushq	%rsi
-	.seh_pushreg	%rsi
+.LFB0:
+	.cfi_startproc
+	endbr64
+	leaq	-8(%rsi), %rax
+	cmpq	%rdi, %rax
+	jbe	.L18
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
 	pushq	%rbx
-	.seh_pushreg	%rbx
-	subq	$40, %rsp
-	.seh_stackalloc	40
-	.seh_endprologue
-	movq	%rdx, %rsi
-	leaq	-8(%rdx), %rax
-	cmpq	%rcx, %rax
-	jbe	.L1
-	subq	%rcx, %rdx
-	sarq	$3, %rdx
-	movq	%rdx, %r8
-	shrq	$63, %rdx
-	addq	%r8, %rdx
-	sarq	%rdx
-	movq	(%rcx,%rdx,8), %r8
-	movq	%rcx, %rbx
-.L3:
-	movq	(%rbx), %rdx
-	cmpq	%rdx, %r8
-	jle	.L8
-.L4:
-	addq	$8, %rbx
-	movq	(%rbx), %rdx
-	cmpq	%r8, %rdx
-	jl	.L4
-.L8:
-	movq	(%rax), %r9
-	cmpq	%r9, %r8
-	jge	.L5
-.L6:
-	subq	$8, %rax
-	movq	(%rax), %r9
-	cmpq	%r8, %r9
-	jg	.L6
-.L5:
-	cmpq	%rax, %rbx
-	jbe	.L12
-.L7:
-	leaq	8(%rax), %rdx
-	call	sort
+	.cfi_def_cfa_offset 24
+	.cfi_offset 3, -24
+	subq	$8, %rsp
+	.cfi_def_cfa_offset 32
+	movq	%rsi, %rbp
 	movq	%rsi, %rdx
-	movq	%rbx, %rcx
-	call	sort
-	nop
-.L1:
-	addq	$40, %rsp
-	popq	%rbx
-	popq	%rsi
-	ret
-.L12:
-	movq	%r9, (%rbx)
-	movslq	%edx, %rdx
+	subq	%rdi, %rdx
+	movq	%rdx, %rcx
+	sarq	$3, %rcx
+	shrq	$63, %rdx
+	addq	%rcx, %rdx
+	sarq	%rdx
+	movq	(%rdi,%rdx,8), %rcx
+	movq	%rdi, %rbx
+	jmp	.L7
+.L11:
+	movq	%rsi, (%rbx)
 	movq	%rdx, (%rax)
 	addq	$8, %rbx
 	subq	$8, %rax
-	cmpq	%rax, %rbx
-	jbe	.L3
 	jmp	.L7
-	.seh_endproc
-	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+.L8:
+	leaq	8(%rax), %rsi
+	call	sort
+	movq	%rbp, %rsi
+	movq	%rbx, %rdi
+	call	sort
+	addq	$8, %rsp
+	.cfi_remember_state
+	.cfi_def_cfa_offset 24
+	popq	%rbx
+	.cfi_def_cfa_offset 16
+	popq	%rbp
+	.cfi_def_cfa_offset 8
+	ret
+.L21:
+	.cfi_restore_state
+	movq	(%rax), %rsi
+	cmpq	%rsi, %rcx
+	jge	.L11
+.L6:
+	subq	$8, %rax
+	movq	(%rax), %rsi
+	cmpq	%rcx, %rsi
+	jg	.L6
+.L5:
+	cmpq	%rbx, %rax
+	jnb	.L11
+.L7:
+	cmpq	%rax, %rbx
+	ja	.L8
+	movq	(%rbx), %rdx
+	cmpq	%rdx, %rcx
+	jle	.L21
+.L4:
+	addq	$8, %rbx
+	movq	(%rbx), %rdx
+	cmpq	%rcx, %rdx
+	jl	.L4
+	movq	(%rax), %rsi
+	cmpq	%rsi, %rcx
+	jl	.L6
+	jmp	.L5
+.L18:
+	.cfi_def_cfa_offset 8
+	.cfi_restore 3
+	.cfi_restore 6
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sort, .-sort
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
